f331eff29f2fea8330905f332eb55576ff4dcf9b
anasknbar <knbaranasgithub@gmail.com>
2024-10-07 12:46:27 +0000

    Next.js Chrome Extention Template

--- myOut/next_push.js
+++
@@ -1,3 +0,0 @@
-// next_push.js
-(self.__next_f = self.__next_f || []).push([0]);
-self.__next_f.push([2, null]);
--- myOut/next_style.js
+++
@@ -1,5 +0,0 @@
-// next_style.js
-self.__next_f.push([
-  1,
-  '1:HL["/./_next/static/css/c64396be09805dbc.css","style"]\n',
-]);
